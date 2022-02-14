c8f090ae85a6eed6eb2b195bb6b7b7cdfd74b7d7
AnvaUserBot <98270034+shakoHSYNV@users.noreply.github.com>
2022-02-15 02:58:07 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	token: "5240844969:AAFctpnzMBtTQNCbRyv1fkY3OKvH552kUeg",
+	botId: 5240844969, //first numbers of BOT_TOKEN
+	rounds: 20, //rounds amount
+	waitDelay: 12000, //1 round duration
+	timerSteps: 4, //timer indicators
+}
